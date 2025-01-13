4f8f48c4a23d41927b24125957017a3c4519fe82
AKKSHAAT <akkshaat@gmail.com>
2025-01-13 09:51:21 +0530

    Add SearchForm and SearchFormReset components; update Navbar and layout; enhance Next.js configuration and add date formatting utility

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,13 @@
 import { ReactNode } from "react";
-import Navbar from "../components/Navbar";
+import Navbar from "../../components/Navbar";
 
-export default function layout({ children }: Readonly<{children: ReactNode}>) {
-    return (
-        <main className="font-work-sans bg-gray-100 text-gray-800">
-            <Navbar />
-            {children}
-        </main>
-    )
+export default function layout({
+  children,
+}: Readonly<{ children: ReactNode }>) {
+  return (
+    <main className="font-work-sans bg-gray-100 text-gray-800">
+      <Navbar />
+      {children}
+    </main>
+  );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,50 @@
 import Image from "next/image";
+import SearchForm from "../../components/SearchForm";
+import StartupCard from "../components/StartupCard";
 
-export default function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+  const posts = [
+    {
+      _createdAt: "2021-09-01T00:00:00Z",
+      views: 55,
+      author: { id: 1, name:"robot" },
+      _id: 1,
+      description: "This is a pitch",
+      image: "https://placehold.co/600x400",
+      category: { name: "Tech" },
+      title: "We Robots",
+    },
+    
+  ];
   return (
     <>
-     <h1 className="heading ">HOME</h1>
+      <section className="pink_container">
+        <h1 className="heading ">Pitch Your Startup</h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit ideas, VOte on Pitches, and Get Noticed
+        </p>
+        <SearchForm query={query} />
+      </section>
+      <section className="section-container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "Latest Pitches"}
+        </p>
+
+        <ul className="mt-7 grid md:grid-cols-3 sm:grid-cols-2 gap-5 p-4">
+          {posts?.length > 0 ? (
+            posts.map((post, i: number) => ( 
+            <StartupCard key={post?._id}  post={post} />
+          ))
+          ) : (
+            <p className="no-results">No startups found ⚠️</p>
+          )}
+        </ul>
+      </section>
     </>
   );
 }
